a866ff2a6d465457e4de9a5c85412a26454e9eb8
Theo <theo@magnor.as>
2025-03-13 17:54:17 +0100

    Removed unnesisary bindings

--- addons/ubat/src/lib.rs
+++ addons/ubat/src/lib.rs
@@ -1,34 +1,13 @@
 use godot::prelude::*;
 
-mod core;
-mod networking;
+// mod core;
+// mod networking;
 mod resource;
-mod terrain;
-
-// Bring your main classes into scope.
-use core::game_manager::game_manager;
-use networking::network_manager::network_manager;
-use networking::object_manager::object_manager;
-use networking::physics_manager::physics_manager;
-use networking::world_manager::world_manager;
-use resource::resource_manager::resource_manager;
-use terrain::terrain_manager::terrain_manager;
+// mod terrain;
 
 // The entry point of your extension library.
 struct UbatExtension;
 
+
 #[gdextension]
-unsafe impl ExtensionLibrary for UbatExtension {
-    fn on_level_init(level: InitLevel) {
-        if level == InitLevel::Scene {
-            // Register all your classes here
-            godot::register_class::<game_manager>();
-            godot::register_class::<network_manager>();
-            godot::register_class::<object_manager>();
-            godot::register_class::<physics_manager>();
-            godot::register_class::<world_manager>();
-            godot::register_class::<resource_manager>();
-            godot::register_class::<terrain_manager>();
-        }
-    }
-}
+unsafe impl ExtensionLibrary for UbatExtension {}
