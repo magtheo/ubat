d661cf47e1e3d77d9ad4a63a1f927cec6c810979
Theo <theo@magnor.as>
2025-03-19 17:30:04 +0100

    misc

--- addons/ubat/src/core/mod.rs
+++ addons/ubat/src/core/mod.rs
@@ -2,9 +2,6 @@ pub mod game_manager;
 pub mod event_bus;
 pub mod config_manager;
 pub mod world_manager;
-mod networking;
 
 pub use event_bus::EventBus;
-// pub use game_manager::;
 pub use game_manager::GameManager;
-pub use network_manager::NetworkManager;
