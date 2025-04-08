69d00fa5710c03b5dffa1fbd6cc2611a738743a6
Theo <theo@magnor.as>
2025-04-08 14:27:25 +0200

    +1

--- addons/ubat/src/terrain/mod.rs
+++ addons/ubat/src/terrain/mod.rs
@@ -4,7 +4,6 @@ pub mod chunk_manager;
 pub mod chunk_controller;
 pub mod chunk_storage;
 pub mod biome_manager;
-pub mod world_integration;
 pub mod thread_pool;
 
 pub mod generation_rules;
@@ -15,7 +14,6 @@ pub use chunk_manager::ChunkManager;
 pub use chunk_controller::ChunkController;
 pub use chunk_storage::ChunkStorage;
 pub use biome_manager::BiomeManager;
-pub use world_integration::TerrainWorldIntegration;
 
 pub use thread_pool::ThreadPool;
 
